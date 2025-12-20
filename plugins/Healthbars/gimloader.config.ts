3cad5097c98d2678992ca4271665413c8dcd2986
TheLazySquid <thelazysquid2@gmail.com>
2025-12-20 12:02:32 -0500

    Maybe fix healthbars crash

--- plugins/Healthbars/gimloader.config.ts
+++ plugins/Healthbars/gimloader.config.ts
@@ -7,7 +7,7 @@ export default singleConfig({
     author: "TheLazySquid",
     downloadUrl: "https://raw.githubusercontent.com/Gimloader/client-plugins/refs/heads/main/build/plugins/Healthbars.js",
     webpage: "https://gimloader.github.io/plugins/healthbars",
-    version: "0.1.4",
+    version: "0.1.5",
     gamemodes: ["2d"],
-    changelog: ["Fixed healthbars not updating when health mode changes in Creative"]
+    changelog: ["Fixed potential crash when others join your game"]
 });
