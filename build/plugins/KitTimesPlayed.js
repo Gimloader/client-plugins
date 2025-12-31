630a4de04cc210535c817615b065783ac8d95376
retrozy <202344625+retrozy1@users.noreply.github.com>
2025-12-30 21:36:59 -0500

    Add KitTimesPlayed and replace the old minified util (#26)
    * Add KitTimesPlayed and replace the old minified util
    
    * Hardcode the name

---
+++ build/plugins/KitTimesPlayed.js
@@ -0,0 +1,60 @@
+/**
+ * @name KitTimesPlayed
+ * @description Shows the number of times that kits have been played on the kits screen
+ * @author retrozy
+ * @version 0.1.0
+ * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/refs/heads/main/build/plugins/KitTimesPlayed.js
+ * @webpage https://gimloader.github.io/plugins/kittimesplayed
+ * @reloadRequired notingame
+ */
+
+// shared/rewritingUtils.ts
+function getRange(code, match) {
+  const snippets = [];
+  let currentWord = "";
+  for (const letter of match) {
+    if (letter === "#") {
+      snippets.push(currentWord);
+      currentWord = "";
+    } else if (letter === "@") {
+      snippets.push(currentWord);
+      currentWord = "";
+      snippets.push("@");
+    } else {
+      currentWord += letter;
+    }
+  }
+  snippets.push(currentWord);
+  const matchIndex = snippets.indexOf("@");
+  const snippetsBeforeMatch = snippets.slice(0, matchIndex);
+  let startIndex = 0;
+  for (const snippet of snippetsBeforeMatch) {
+    startIndex = code.indexOf(snippet, startIndex) + snippet.length;
+  }
+  const snippetAfterMatch = snippets[matchIndex + 1];
+  const endIndex = code.indexOf(snippetAfterMatch, startIndex);
+  return {
+    startIndex,
+    endIndex
+  };
+}
+function getSection(code, match) {
+  const { startIndex, endIndex } = getRange(code, match);
+  return code.slice(startIndex, endIndex);
+}
+function insert(code, match, string) {
+  const { endIndex } = getRange(code, match);
+  const start = code.slice(0, endIndex);
+  const end = code.slice(endIndex);
+  return start + string + end;
+}
+
+// plugins/KitTimesPlayed/src/index.ts
+api.rewriter.addParseHook("Container", (code) => {
+  if (!code.includes("There are no kits in this folder.")) return code;
+  const name = getSection(code, ".createdAt?`Created ${#(@.");
+  const playCountString = `${name}.playCount`;
+  const playCountInfo = `, \${${playCountString} ? \`played \${${playCountString}} \${${playCountString} === 1 ? "time" : "times"}\` : "never played"}`;
+  const isEnabledString = `GL.plugins.isEnabled("KitTimesPlayed")`;
+  return insert(code, ".createdAt).fromNow()}@`", `\${${isEnabledString} ? \`${playCountInfo}\` : ""}`);
+});
--- plugins/NoSchoolHours/src/index.ts
+++ plugins/NoSchoolHours/src/index.ts
@@ -1,9 +1,7 @@
+import { replaceSection } from "$shared/rewritingUtils";
+
 api.rewriter.addParseHook("App", code => {
     if(!code.includes("Discovery Is Closed During School Hours")) return code;
 
-    const beforeVarIndex = code.indexOf("()&&") + 4;
-    const afterVarIndex = code.indexOf("?", beforeVarIndex);
-    const start = code.slice(0, beforeVarIndex);
-    const end = code.substring(afterVarIndex);
-    return start + "false" + end;
+    return replaceSection(code, "()&&@?", "false");
 });
