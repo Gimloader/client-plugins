f2bdfc3ba0a0382dc34cc75884b2910653a53e78
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-01-25 13:38:04 -0500

    [KitTimesPlayed] Instantly cleanup when disabled

--- build/plugins/KitTimesPlayed.js
+++ build/plugins/KitTimesPlayed.js
@@ -58,3 +58,12 @@ api.rewriter.addParseHook("Container", (code) => {
   const isEnabledString = `GL.plugins.isEnabled("KitTimesPlayed")`;
   return insert(code, ".createdAt).fromNow()}@`", `\${${isEnabledString} ? \`${playCountInfo}\` : ""}`);
 });
+api.onStop(() => {
+  if (location.pathname !== "/kits" && location.pathname !== "/gamemode") return;
+  const kits = document.getElementsByClassName("ant-card ant-card-bordered ant-card-hoverable");
+  for (const kit of kits) {
+    const div = kit.children[0]?.children[0]?.children[0]?.children[1]?.children[1];
+    if (!div?.innerHTML.includes(",")) return;
+    div.innerHTML = div.innerHTML.split(",")[0];
+  }
+});
