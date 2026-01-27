517327d4cf7f01237e65b8c7feea3465fb2a6a58
TheLazySquid <thelazysquid2@gmail.com>
2026-01-27 18:46:16 -0500

    Version bump

--- build/plugins/KitTimesPlayed.js
+++ build/plugins/KitTimesPlayed.js
@@ -2,7 +2,7 @@
  * @name KitTimesPlayed
  * @description Shows the number of times that kits have been played on the kits screen
  * @author retrozy
- * @version 0.1.0
+ * @version 0.1.1
  * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/refs/heads/main/build/plugins/KitTimesPlayed.js
  * @webpage https://gimloader.github.io/plugins/kittimesplayed
  * @reloadRequired notingame
