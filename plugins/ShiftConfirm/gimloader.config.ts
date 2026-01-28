39e1cb05fda1ed116e07b8b6aa650215e8f4e73e
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-01-27 22:38:28 -0500

    Add ShiftConfirm

---
+++ plugins/ShiftConfirm/gimloader.config.ts
@@ -0,0 +1,11 @@
+import { singleConfig } from "@gimloader/build";
+
+export default singleConfig({
+    input: "src/index.ts",
+    name: "ShiftConfirm",
+    description: "Makes confirm popups resolve instantly when holding shift.",
+    author: "retrozy",
+    downloadUrl: "https://raw.githubusercontent.com/Gimloader/client-plugins/main/build/plugins/ShiftConfirm.js",
+    webpage: "https://gimloader.github.io/plugins/shiftconfirm",
+    version: "0.1.0"
+});
