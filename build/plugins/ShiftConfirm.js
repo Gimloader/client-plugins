3eabbe69c50a5aa15812fc07bcf058fb0bbd2261
retrozy <202344625+retrozy1@users.noreply.github.com>
2026-01-31 19:20:54 -0500

    Add ShiftConfirm (#46)
    * Add ShiftConfirm
    
    * Use hotkeys api and modal type
    
    * Fix key name

---
+++ build/plugins/ShiftConfirm.js
@@ -0,0 +1,32 @@
+/**
+ * @name ShiftConfirm
+ * @description Makes confirm popups resolve instantly when holding shift.
+ * @author retrozy
+ * @version 0.1.0
+ * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/main/build/plugins/ShiftConfirm.js
+ * @webpage https://gimloader.github.io/plugins/shiftconfirm
+ */
+
+// plugins/ShiftConfirm/src/index.ts
+api.rewriter.exposeVar(true, {
+  find: /(\S+)\.useModal=\S+;\1\.info=function/,
+  callback(modal) {
+    const originalConfirm = modal.confirm;
+    modal.confirm = (props) => {
+      if (api.hotkeys.pressed.has("ShiftLeft")) {
+        props.onOk?.();
+        return {
+          destroy() {
+          },
+          update() {
+          }
+        };
+      } else {
+        return originalConfirm(props);
+      }
+    };
+    api.onStop(() => {
+      modal.confirm = originalConfirm;
+    });
+  }
+});
