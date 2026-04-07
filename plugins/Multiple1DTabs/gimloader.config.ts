1b11249fc9754d631a1f7c951a699f7cee691a36
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-04-07 17:47:07 -0400

    Add Multiple1DTabs

---
+++ plugins/Multiple1DTabs/gimloader.config.ts
@@ -0,0 +1,9 @@
+import { officialScriptConfig } from "$shared/config";
+
+export default officialScriptConfig({
+    input: "src/index.ts",
+    name: "Multiple1DTabs",
+    description: "Allows having multiple different tabs connected to a single 1D game",
+    author: "retrozy",
+    version: "0.1.0"
+});
