c2b87c3838849fa0751fcc9e01a6cad59612f2a5
retrozy <202344625+retrozy1@users.noreply.github.com>
2026-04-12 19:13:25 -0400

    Update TypeScript (#77)
    * Update TypeScript
    
    * Remove unneeded export usages
    
    * Update svelte-check
    
    ---------
    
    Co-authored-by: TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>

--- main.d.ts
+++ main.d.ts
@@ -1,17 +1,22 @@
 declare namespace Gimloader {
     interface Plugins {
-        Savestates: typeof import("plugins/Savestates/src");
-        InputRecorder: typeof import("plugins/InputRecorder/src");
-        Desynchronize: typeof import("plugins/Desynchronize/src");
-        DLDTAS: typeof import("plugins/DLDTAS/src");
+        Savestates: typeof import("./plugins/Savestates/src");
+        InputRecorder: typeof import("./plugins/InputRecorder/src");
+        Desynchronize: typeof import("./plugins/Desynchronize/src");
+        DLDTAS: typeof import("./plugins/DLDTAS/src");
     }
     
     interface Libraries {
-        Communication: typeof import("libraries/Communication/src").default;
+        Communication: typeof import("./libraries/Communication/src").default;
     }
 }
 
 declare module "*.scss" {
+    const content: string;
+    export default content;
+}
+
+declare module '*.txt' {
     const content: string;
     export default content;
 }
