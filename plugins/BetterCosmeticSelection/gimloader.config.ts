d2cc4405eebc95d2cd42b0b7cc2fb9f6d085b4ec
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-07-11 14:10:36 -0400

    Add BetterCosmeticSelection

---
+++ plugins/BetterCosmeticSelection/gimloader.config.ts
@@ -0,0 +1,8 @@
+import { officialScriptConfig } from "$shared/config";
+
+export default officialScriptConfig({
+    name: "BetterCosmeticSelection",
+    description: "Allows you to select your cosmetics by only clicking on them.",
+    input: "src/index.ts",
+    reloadRequired: true
+});
