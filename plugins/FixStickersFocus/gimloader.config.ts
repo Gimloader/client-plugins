54a514e1d1e8fa3947733deddc60f4ecf59483d7
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-07-11 16:30:30 -0400

    Add version

--- plugins/FixStickersFocus/gimloader.config.ts
+++ plugins/FixStickersFocus/gimloader.config.ts
@@ -4,5 +4,6 @@ export default officialScriptConfig({
     name: "FixStickersFocus",
     description: "Fixes the in-game stickers button keeping focus after being closed",
     input: "src/index.ts",
-    reloadRequired: "ingame"
+    reloadRequired: "ingame",
+    version: "0.1.0"
 });
