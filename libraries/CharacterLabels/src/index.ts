52cbb73a8b92d10363365295e7034ee2ed9ee01c
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-07-22 21:52:10 -0400

    Implement CharacterLabels changes

--- libraries/CharacterLabels/src/index.ts
+++ libraries/CharacterLabels/src/index.ts
@@ -1,5 +1,9 @@
-const padding = 10;
-type LabelObject = Phaser.GameObjects.GameObject & Phaser.GameObjects.Components.Transform & Phaser.GameObjects.Components.Depth & Phaser.GameObjects.Components.Visible;
+type LabelObject =
+    & Phaser.GameObjects.GameObject
+    & Phaser.GameObjects.Components.Transform
+    & Phaser.GameObjects.Components.Depth
+    & Phaser.GameObjects.Components.Visible
+    & { height: number };
 
 export interface LabelInstance {
     gameObjects: LabelObject[];
@@ -8,112 +12,116 @@ export interface LabelInstance {
 
 export type SetupLabel = (character: Gimloader.Stores.Character) => LabelInstance | void;
 
-let addLabelToGame: ((setupLabel: SetupLabel) => () => void) | null = null;
-
-export default function addLabel(setupLabel: SetupLabel) {
-    if(!addLabelToGame) throw new Error("Labels cannot be added before loading in");
-    return addLabelToGame(setupLabel);
+export interface LabelCreator {
+    setupLabel: SetupLabel;
+    destroyers: Map<Gimloader.Stores.Character, () => void>;
 }
 
-interface Label {
-    heights: Map<Gimloader.Stores.Character, number>;
-    setupLabel: SetupLabel;
-    onStop: Set<() => void>;
+const labelCreators: LabelCreator[] = [];
+const characterLabels = new Map<Gimloader.Stores.Character, LabelInstance[]>();
+const padding = 10;
+
+export default function addLabel(setupLabel: SetupLabel) {
+    const creator: LabelCreator = { setupLabel, destroyers: new Map() };
+    labelCreators.push(creator);
+
+    // Add labels to existing characters
+    for(const [character, labelInstances] of characterLabels) {
+        const onStop = setupForCharacter(character, setupLabel, labelInstances);
+        if(onStop) creator.destroyers.set(character, onStop);
+    }
+
+    return () => {
+        for(const destroy of creator.destroyers.values()) destroy();
+
+        const index = labelCreators.indexOf(creator);
+        if(index === -1) return;
+        labelCreators.splice(index, 1);
+    };
+}
+
+function setupForCharacter(character: Gimloader.Stores.Character, setupLabel: SetupLabel, instances: LabelInstance[]) {
+    const label = setupLabel(character);
+    if(!label) return;
+
+    instances.push(label);
+
+    return () => {
+        for(const object of label.gameObjects) object.destroy();
+
+        const index = instances.indexOf(label);
+        if(index === -1) return;
+        instances.splice(index, 1);
+    };
+}
+
+function getMaxHeight(objects: LabelObject[]) {
+    let labelHeight = 0;
+
+    for(const object of objects) {
+        if(("visible" in object && !object.visible) || !("height" in object)) continue;
+        if(object.height > labelHeight) labelHeight = object.height;
+    }
+
+    return labelHeight;
+}
+
+function patchCharacter(character: Gimloader.Stores.Character) {
+    if(!characterLabels.has(character)) characterLabels.set(character, []);
+    const labels = characterLabels.get(character)!;
+
+    for(const creator of labelCreators) {
+        const onStop = setupForCharacter(character, creator.setupLabel, labels);
+        if(onStop) creator.destroyers.set(character, onStop);
+    }
+
+    const unpatchUpdate = api.patcher.after(character.nametag, "update", () => {
+        if(!character.nametag.tag) return;
+        const { x, y, depth } = character.nametag.tag;
+        const stateChar = api.net.state.characters.get(character.id);
+
+        let offset = 22;
+        if(character.nametag.fragilityTag) offset += 15;
+
+        for(const label of labels) {
+            for(const object of label.gameObjects) {
+                object.visible = true;
+                object.x = x;
+                object.y = y + offset;
+                object.setDepth(depth);
+            }
+
+            label.update?.();
+
+            if(stateChar?.isRespawning || stateChar?.teamId === "__SPECTATORS_TEAM") {
+                for(const object of label.gameObjects) {
+                    object.visible = false;
+                }
+            }
+
+            offset += getMaxHeight(label.gameObjects) + padding;
+        }
+    });
+
+    const unpatchDestroy = api.patcher.after(character.nametag, "destroy", () => {
+        for(const creator of labelCreators) {
+            creator.destroyers.get(character)?.();
+            creator.destroyers.delete(character);
+        }
+
+        unpatchUpdate();
+        unpatchDestroy();
+    });
 }
 
 api.net.onLoad(() => {
-    const handleLabel = (character: Gimloader.Stores.Character, label: Label) => {
-        const stateChar = api.net.state.characters.get(character.id);
-        const labelInstance = label.setupLabel(character);
-        if(!labelInstance) return;
+    const characterManager = api.stores.phaser.scene.characterManager;
 
-        const getHeight = () => {
-            let maxHeight = 0;
-            for(const object of labelInstance.gameObjects) {
-                if(("visible" in object && !object.visible) || !("height" in object)) continue;
-                maxHeight = Math.max(maxHeight, object.height as number);
-            }
-            return maxHeight;
-        };
-
-        const getYOffset = () => {
-            let offset = 22;
-            if(character.nametag.fragilityTag) {
-                offset += 15;
-            }
-            for(const characterLabel of labels.slice(0, labels.indexOf(label))) {
-                const height = characterLabel.heights.get(character)!;
-                if(height) offset += padding;
-                offset += height;
-            }
-            return offset;
-        };
-
-        const unsub = api.patcher.after(character.nametag, "update", () => {
-            if(!character.nametag.tag) return;
-            for(const addedObject of labelInstance.gameObjects) {
-                const { x, y, depth } = character.nametag.tag;
-                addedObject.visible = true;
-                addedObject.x = x;
-                addedObject.y = y + getYOffset();
-                addedObject.setDepth(depth);
-            }
-
-            labelInstance.update?.();
-            if(stateChar?.isRespawning || stateChar?.teamId === "__SPECTATORS_TEAM") {
-                for(const addedObject of labelInstance.gameObjects) {
-                    addedObject.visible = false;
-                }
-            }
-            const height = getHeight();
-            label.heights.set(character, height);
-        });
-
-        const stopDestroy = api.patcher.after(character.nametag, "destroy", () => {
-            destroy();
-            label.onStop.delete(destroy);
-        });
-
-        const destroy = () => {
-            unsub();
-            for(const object of labelInstance.gameObjects) {
-                object.destroy();
-            }
-            stopDestroy();
-        };
-
-        label.onStop.add(destroy);
-    };
-
-    const scene = api.stores.phaser.scene;
-
-    const labels: Label[] = [];
-    addLabelToGame = (setupLabel) => {
-        const label: Label = {
-            setupLabel,
-            onStop: new Set(),
-            heights: new Map()
-        };
-        labels.push(label);
-
-        for(const character of api.stores.phaser.scene.characterManager.characters.values()) {
-            handleLabel(character, label);
-        }
-
-        return () => {
-            for(const cb of label.onStop) {
-                cb();
-            }
-
-            const index = labels.indexOf(label);
-            if(index === -1) return;
-            labels.splice(index, 1);
-        };
-    };
-
-    api.patcher.after(scene.characterManager, "addCharacter", (_, __, character) => {
-        for(const label of labels) {
-            handleLabel(character, label);
-        }
+    api.patcher.after(characterManager, "addCharacter", (_, __, character) => {
+        patchCharacter(character);
     });
+
+    for(const character of characterManager.characters.values()) {
+        patchCharacter(character);
+    }
 });
