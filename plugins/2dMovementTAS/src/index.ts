be98d4a2f0d0f976025bd41b20d36841d620368d
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-07-29 20:26:01 +0200

    Unlist 2dMovementTAS

--- plugins/2dMovementTAS/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { mount, unmount } from "svelte";
-import UI from "./ui/Start.svelte";
-
-api.net.onLoad(() => {
-    const ui = mount(UI, { target: document.body });
-
-    api.onStop(() => unmount(ui));
-});
