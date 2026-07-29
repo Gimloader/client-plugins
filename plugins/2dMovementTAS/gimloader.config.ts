5b1e58660fb8f4020d2684f4c246691b040e795e
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-07-29 20:20:23 +0200

    Deprecate 2dMovementTAS

--- plugins/2dMovementTAS/gimloader.config.ts
+++ plugins/2dMovementTAS/gimloader.config.ts
@@ -4,8 +4,9 @@ export default officialScriptConfig({
     input: "src/index.ts",
     name: "2dMovementTAS",
     description: "Allows for making TASes of CTF and tag",
-    version: "0.4.1",
+    version: "0.4.2",
     reloadRequired: "ingame",
     gamemodes: ["ctf", "tag"],
-    changelog: ["Updated webpage url"]
+    changelog: ["Deprecated plugin"],
+    deprecated: "This plugin has been broken by changes to Gimkit and there are not currently any plans to fix it."
 });
