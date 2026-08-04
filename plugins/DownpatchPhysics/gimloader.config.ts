212c82b83a81dcd19ea9cd1d28c568004809089e
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-08-04 09:13:46 +0200

    Remove reloadRequired

--- plugins/DownpatchPhysics/gimloader.config.ts
+++ plugins/DownpatchPhysics/gimloader.config.ts
@@ -4,10 +4,9 @@ export default officialScriptConfig({
     input: "./src/index.ts",
     name: "DownpatchPhysics",
     description: "Restore physics to how it functioned in older versions of Gimkit",
-    version: "0.1.1",
+    version: "0.1.2",
     hasSettings: true,
     needsPlugins: ["Desynchronize"],
     gamemodes: ["2d"],
-    reloadRequired: "ingame",
     changelog: ["Accomodated new Gimkit update"]
 });
