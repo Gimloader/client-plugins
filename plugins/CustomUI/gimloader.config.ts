b7810239913150033d89f28ba2e9f71ed341ba76
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-08-30 18:00:58 -0400

    Fix theme sometimes being displayed when disabled

--- plugins/CustomUI/gimloader.config.ts
+++ plugins/CustomUI/gimloader.config.ts
@@ -4,7 +4,7 @@ export default officialScriptConfig({
     input: "src/index.ts",
     name: "CustomUI",
     description: "Allows you to customize various things about the Gimkit UI",
-    version: "0.3.4",
+    version: "0.3.5",
     hasSettings: true,
-    changelog: ["Updated webpage url"]
+    changelog: ["Fixed theme displaying on 1d answer screen even when disabled"]
 });
